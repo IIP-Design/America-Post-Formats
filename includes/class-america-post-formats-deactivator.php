0952e947c81f23bf8b0ed86123febebe4d1ac8af
Nathan Kleekamp <nkleekamp@gmail.com>
2016-06-08 14:26:13 -0400

    initial commit

---
+++ includes/class-america-post-formats-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    America_Post_Formats
+ * @subpackage America_Post_Formats/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    America_Post_Formats
+ * @subpackage America_Post_Formats/includes
+ * @author     Your Name <email@example.com>
+ */
+class America_Post_Formats_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
